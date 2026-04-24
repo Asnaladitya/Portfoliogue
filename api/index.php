16c4b234bde5e20298128a9f53dd22ef1aaf4c77
Asnaladitya <asnalaja1@gmail.com>
2026-04-24 22:13:50 +0700

    test: testing simple php output

--- api/index.php
+++ api/index.php
@@ -1,16 +1,3 @@
 <?php
-
-// Menampilkan error secara paksa agar tidak cuma 500
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// Pastikan folder storage bootstrap ada
-$storagePath = '/tmp/storage/bootstrap/cache';
-if (!is_dir($storagePath)) {
-    mkdir($storagePath, 0755, true);
-}
-
-putenv("CACHE_CONFIGPATH=$storagePath");
-
-require __DIR__ . '/../public/index.php';
+echo "Halo Asnal! PHP di Vercel sudah hidup.";
+// phpinfo(); // Hapus tanda // di depan kalau mau lihat spek lengkap
