771051b9785e0dd95676e33c7a8860e0fc77c666
Asnaladitya <asnalaja1@gmail.com>
2026-04-24 21:58:24 +0700

    environment update

--- api/index.php
+++ api/index.php
@@ -1,2 +1,16 @@
 <?php
+
+// Menampilkan error secara paksa agar tidak cuma 500
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Pastikan folder storage bootstrap ada
+$storagePath = '/tmp/storage/bootstrap/cache';
+if (!is_dir($storagePath)) {
+    mkdir($storagePath, 0755, true);
+}
+
+putenv("CACHE_CONFIGPATH=$storagePath");
+
 require __DIR__ . '/../public/index.php';
